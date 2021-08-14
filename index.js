77ca17476b596a136048045bcb0e686fbbc7b190
Rutuja14 <>
2021-08-14 13:27:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var read=require('readline-sync');
+
+var username=read.question("Can I have your name? ")
+console.log("Welcome "+ username)
+score=0
+function quiz(ques,ans){
+  userAnswer=read.question(ques)
+  if (userAnswer.toLowerCase()==ans.toLowerCase()){
+    console.log("Right")
+    score=score+1
+  }
+  else{
+    console.log("Wrong")
+  }
+  console.log("Your current score is "+score)
+}
+question=[{
+  ques:"Favourite color ",
+  ans:"Lilac"},
+{
+  ques:"Favourite food ",
+  ans:"Paneer",
+}]
+for(i=0;i<question.length;i++){
+  curr=question[i]
+  quiz(curr.ques,curr.ans)
+}
+console.log("Final score is "+score)
